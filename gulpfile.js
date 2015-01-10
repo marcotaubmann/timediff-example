dad1d059d3ef15d8f5eb38903cbd083acbf5820e
Marco Taubmann <marco.taubmann@gmail.com>
2015-01-10 02:12:09 +0100

    Add build target dist

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,25 @@ var notify = require('gulp-notify');
 var jshint = require('gulp-jshint');
 var concat = require('gulp-concat');
 var inject = require('gulp-inject');
+var uglify = require('gulp-uglify');
+var minifyCss = require('gulp-minify-css');
+var minifyHtml = require('gulp-minify-html');
+var at = require('gulp-asset-transform');
+var gulpFilter = require('gulp-filter');
 var bowerFiles = require('main-bower-files');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
+var sizereport = require('gulp-sizereport');
 
 gulp.task('clean', function (callback) {
-  del('build/**', callback);
+  del([
+    'build/**',
+    'dist/**'
+  ], callback);
 });
 
 var appFileStream;
-gulp.task('app', function (callback) {
+gulp.task('scripts', function (callback) {
   appFileStream = gulp
     .src('app/**/*.js')
     .pipe(jshint())
@@ -23,8 +32,13 @@ gulp.task('app', function (callback) {
   callback();
 });
 
-gulp.task('styles', function () {
-
+var styleFileStream;
+gulp.task('styles', function (callback) {
+  styleFileStream = gulp
+    .src('app/**/*.css')
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest('build'));
+  callback();
 });
 
 var vendorFilesStream;
@@ -37,11 +51,12 @@ gulp.task('vendor', function (callback) {
   callback();
 });
 
-gulp.task('index', ['app', 'styles', 'vendor'], function () {
+gulp.task('index', ['scripts', 'styles', 'vendor'], function () {
   return gulp
     .src(['app/index.html'])
     // write here first for correct relative injection path
     .pipe(gulp.dest('build'))
+    .pipe(inject(styleFileStream, {name: 'style', relative: true}))
     .pipe(inject(appFileStream, {name: 'app', relative: true}))
     .pipe(inject(vendorFilesStream, {name: 'vendor', relative: true}))
     // write here again with injected files
@@ -64,8 +79,34 @@ gulp.task('serve', ['build'], function (callback) {
 });
 
 gulp.task('watch', ['serve'], function () {
-  gulp.watch(['app/**/*.js'], ['app']);
+  gulp.watch(['app/**/*.js'], ['scripts']);
+  gulp.watch(['app/**/*.css'], ['styles']);
+  gulp.watch(['bower.json'], ['index']);
   gulp.watch(['app/index.html'], ['index']);
 
   gulp.watch(['build/**'], reload);
 });
+
+gulp.task('dist', ['build'], function () {
+  var htmlFilter = gulpFilter ('*.html');
+  return gulp
+    .src(['build/index.html'])
+    .pipe(at({
+      css: { stream: function (filestream, outputFilename)  {
+        return filestream
+          .pipe(minifyCss())
+          .pipe(concat(outputFilename));
+      }},
+      js: { stream: function (filestream, outputFilename)  {
+        return filestream
+          .pipe(uglify())
+          .pipe(concat(outputFilename));
+      }}
+    }))
+    .pipe(htmlFilter)
+    .pipe(minifyHtml())
+    .pipe(htmlFilter.restore())
+    .pipe(gulp.dest('dist'))
+    .pipe(sizereport({gzip: true}));
+});
+
